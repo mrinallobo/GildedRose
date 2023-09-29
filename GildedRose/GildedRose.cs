6c4098883df415be58b9712a822637dce8fb3d2c
mrinallobo <mrinal6lobo@gmail.com>
2023-09-29 10:28:43 +0530

    First commit

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,118 @@
+﻿using GildedRoseKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GildedRose
+{
+    public class GildedRose
+    {
+        private IList<Item> Items;
+
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                var itemUpdater = ItemUpdaterFactory.CreateItemUpdater(item);
+                itemUpdater.UpdateItem(item);
+            }
+        }
+    }
+
+    public interface IItemUpdater
+    {
+        void UpdateItem(Item item);
+    }
+    public class RegularItemUpdater : IItemUpdater
+    {
+        public void UpdateItem(Item item)
+        {
+            item.SellIn--;
+            item.Quality = DecreaseQuality(item.SellIn, item.Quality);
+            item.Quality = Math.Max(item.Quality, 0);
+        }
+        private int DecreaseQuality(int sellIn, int quality)
+        {
+            // Decrease quality by 1 if sellIn >= 0, else decrease by 2
+            return sellIn >= 0 ? quality - 1 : quality - 2;
+        }
+    }
+
+    public class ConjuredItem : IItemUpdater
+    {
+        public void UpdateItem(Item item)
+        {
+            item.SellIn--;
+            item.Quality = DecreaseQuality(item.SellIn, item.Quality);
+            item.Quality = Math.Max(item.Quality, 0);
+        }
+        private int DecreaseQuality(int sellIn, int quality)
+        {
+            return sellIn >= 0 ? quality - 2 : quality - 4;
+        }
+    }
+    public class AgedBrieItemUpdater : IItemUpdater
+    {
+        public void UpdateItem(Item item)
+        {
+            item.SellIn--;
+            item.Quality = IncreaseQuality(item.SellIn, item.Quality);
+            item.Quality = Math.Min(item.Quality, 50);
+        }
+        private int IncreaseQuality(int sellIn, int quality)
+        {
+            // Increase quality by 1 if sellIn >= 0, else increase by 2
+            return sellIn >= 0 ? quality + 1 : quality + 2;
+        }
+    }
+    public class BackstagePassItemUpdater : IItemUpdater
+    {
+        public void UpdateItem(Item item)
+        {
+            item.SellIn--;
+            item.Quality = IncreaseQuality(item.SellIn, item.Quality);
+            item.Quality = Math.Max(item.Quality, 0);
+        }
+        private int IncreaseQuality(int sellIn, int quality)
+        {
+            return sellIn < 0
+                ? 0
+                : sellIn < 5
+                    ? Math.Min(50, quality + 3)
+                    : sellIn < 10
+                        ? Math.Min(50, quality + 2)
+                        : Math.Min(50, quality + 1);
+        }
+
+    }
+
+    public class SulfurasItemUpdater : IItemUpdater
+    {
+        public void UpdateItem(Item item){}
+    }
+
+    public static class ItemUpdaterFactory
+    {
+        private static readonly Dictionary<string, Func<IItemUpdater>> ItemUpdaterFactories = new Dictionary<string, Func<IItemUpdater>>
+        {
+            { "Aged Brie", () => new AgedBrieItemUpdater() },
+            { "Backstage passes to a TAFKAL80ETC concert", () => new BackstagePassItemUpdater() },
+            { "Sulfuras, Hand of Ragnaros", () => new SulfurasItemUpdater() },
+            { "Conjured Mana Cake", () => new ConjuredItem() }
+        };
+
+        public static IItemUpdater CreateItemUpdater(Item item)
+        {
+            if (ItemUpdaterFactories.TryGetValue(item.Name, out var factory))
+            {
+                return factory();
+            }
+            return new RegularItemUpdater();
+        }
+    }
+}
---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,94 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRose;
+using GildedRoseKata;
+
+namespace GildedRoseTests
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            //IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+            //GildedRose app = new GildedRose(Items);
+            //app.UpdateQuality();
+            //Assert.Equal(19, Items[0].Quality);
+            IList<Item> items = new List<Item> {new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+                // this conjured item does not work properly yet
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6} };
+
+
+
+            GildedRose.GildedRose app = new GildedRose.GildedRose(items);
+            for (int i = 0; i < 30; i++)
+                app.UpdateQuality();
+
+
+
+            Assert.Equal(-20, items[0].SellIn);
+            Assert.Equal(0, items[0].Quality);
+
+
+
+            Assert.Equal(-28, items[1].SellIn);
+            Assert.Equal(50, items[1].Quality);
+
+
+
+            Assert.Equal(-25, items[2].SellIn);
+            Assert.Equal(0, items[2].Quality);
+
+
+
+            Assert.Equal(0, items[3].SellIn);
+            Assert.Equal(80, items[3].Quality);
+
+
+
+            Assert.Equal(-1, items[4].SellIn);
+            Assert.Equal(80, items[4].Quality);
+
+
+
+            Assert.Equal(-15, items[5].SellIn);
+            Assert.Equal(0, items[5].Quality);
+
+
+
+            Assert.Equal(-20, items[6].SellIn);
+            Assert.Equal(0, items[6].Quality);
+
+
+
+            Assert.Equal(-25, items[7].SellIn);
+            Assert.Equal(0, items[7].Quality);
+
+
+            Assert.Equal(-27, items[8].SellIn);
+            Assert.Equal(0, items[8].Quality);
+        }
+    }
+}
